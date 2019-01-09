e2161ee0f6b63ee7b6ca9c91f292db6811dd0b0d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 15:57:33 -0800

    calculator.py

--- arithmetic.py
+++ arithmetic.py
@@ -36,3 +36,7 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return int(num1 % num2)
+
+def add_mult(num1, num2, num3):
+    """Return the sum of the first two numbers times the third"""
+    return int((num1 + num2) * num3)
