1531893d89bed65f4fd2872c8d7664af5dbed014
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 17:17:44 -0800

    Compledted REPL part.

--- calculator_2.py
+++ calculator_2.py
@@ -2,21 +2,30 @@
 
 import arithmetic
 
-user_input = input("What would you like to compute? ")
+wants_to_play = True
 
-user_input_split = user_input.split(" ")
+while wants_to_play:
 
-# for item in user_input_split:
-#     print(item)
+    user_input = input("What would you like to compute? (To exit, type 'q'.)")
+    user_input_split = user_input.split(" ")
+    if user_input_split[0] == "q":
+        wants_to_play = False
+    else:
+        print("inside else")
+        if user_input_split[0] == "add":
+            print("Adding")
 
-if user_input_split[0] == "add":
-    print("Adding")
+            x = int(user_input_split[1])
+            y = int(user_input_split[2])
 
-    x = int(user_input_split[1])
-    y = int(user_input_split[2])
+            print(arithmetic.add(x, y))
 
-    print(arithmetic.add(x, y))
+        elif user_input_split[0] == "sub":
+            print("Adding")
 
+            x = int(user_input_split[1])
+            y = int(user_input_split[2])
 
+            print(arithmetic.subtract(x, y))
 
 
