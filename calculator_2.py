3b2e522ec35e963e98f36a9a01f3fbd634d45890
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-09 16:38:17 -0800

    Second commit first function

--- calculator_2.py
+++ calculator_2.py
@@ -1,2 +1,22 @@
 """Math functions for calculator."""
 
+import arithmetic
+
+user_input = input("What would you like to compute? ")
+
+user_input_split = user_input.split(" ")
+
+# for item in user_input_split:
+#     print(item)
+
+if user_input_split[0] == "add":
+    print("Adding")
+
+    x = int(user_input_split[1])
+    y = int(user_input_split[2])
+
+    print(arithmetic.add(x, y))
+
+
+
+
